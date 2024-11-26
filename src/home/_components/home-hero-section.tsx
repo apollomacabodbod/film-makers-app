a2a865a83c7f2462699626578ad69eeb1978138e
apollo <apollomacabodbod8@gmail.com>
2024-11-27 07:38:30 +0800

    added skeleton in hero section

---
+++ src/home/_components/home-hero-section.tsx
@@ -0,0 +1,60 @@
+
+
+
+
+export default function HomeHeroSection(){
+
+
+
+  return (<>
+
+
+    <div className="flex flex-col border border-[#50B498] green">
+
+
+      <p>One scene at a time - the movie maker director.</p>
+
+      <p>Behind Every Great Film is a Visionary.</p>
+
+
+      <p>Crafting unforgettable stories bringing their creative vision to life on the big screen - the movie maker director</p>
+
+    
+      <div className="flex items-center">
+
+
+        <div className="flex flex-col">
+
+          <button className="">Get started</button>
+
+        </div>
+
+
+
+        <div className="flex flex-col">
+
+
+          <div className="flex items-center justify-between gap-[0.8125em]">
+
+            <p>view our works</p>
+
+            <img src="home-hero-section-play.svg" alt="" width={13} height={0} className="min-w-[0.8125em]"/>
+
+          </div>
+
+
+
+        </div>
+
+
+
+
+      </div>
+
+
+       
+    </div>
+    
+    
+  </>)
+}
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,4 +1,5 @@
 import HomeHeader from "./_components/home-header";
+import HomeHeroSection from "./_components/home-hero-section";
 import HomeSmoothScroll from "./_components/home-smooth-scroll";
 
 export default function Home() {
@@ -9,11 +10,12 @@ export default function Home() {
      
       <HomeSmoothScroll/>
 
-      
+
       <div className="flex flex-col border border-[#50B498] green bg-[rgba(26,27,42,0.40)] lg:px-[10.6875em]  px-[1.25em] transition-all duration-1000 ease-in-out">
 
         
         <HomeHeader/>
+        <HomeHeroSection/>
 
 
       </div>
