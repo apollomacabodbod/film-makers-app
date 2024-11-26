e352f9cfd260aae01ef650a3e89cc987e75bea0d
apollo <apollomacabodbod8@gmail.com>
2024-11-26 16:22:05 +0800

    added smooth scroll

---
+++ src/home/_components/home-smooth-scroll.tsx
@@ -0,0 +1,32 @@
+
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+
+
+export default function HomeSmoothScroll(){
+
+  useEffect(() => {
+    // Initialize Lenis for smooth scrolling
+    const lenis = new Lenis({
+      duration: 1.5, // Controls the smoothness
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
+    });
+    
+    // RAF loop for continuous smooth scroll
+    function raf(time: DOMHighResTimeStamp) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    
+    requestAnimationFrame(raf);
+    
+    // Cleanup function to destroy Lenis on unmount
+    return () => lenis.destroy();
+  }, []);
+
+  return null;
+    
+    
+}
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,5 +1,9 @@
+import HomeSmoothScroll from "./_components/home-smooth-scroll";
+
 export default function Home() {
   return (<>
+
+    <HomeSmoothScroll/>
 
     <div className="flex flex-col min-h-screen bg-yellow-800">
 
