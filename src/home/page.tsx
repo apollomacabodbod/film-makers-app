7f57d9e3bb61788925e276824957677c911b65b3
apollo <apollomacabodbod8@gmail.com>
2024-11-27 09:02:37 +0800

    adjust the overlay into 90

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="relative flex flex-col border border-[#50B498] green bg-[rgba(26,27,42,0.40)] lg:px-[10.6875em]  px-[1.25em] transition-all duration-1000 ease-in-out">
 
         {/* Overlay */}
-        <div className="absolute inset-0 bg-[#1A1B2A66] opacity-40"></div>
+        <div className="absolute inset-0 bg-[#1A1B2A66] opacity-90"></div>
 
           
         <HomeHeader/>
