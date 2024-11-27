4622f9e74e4210a913ae42c3cbc856c74b383e89
apollo <apollomacabodbod8@gmail.com>
2024-11-27 10:02:26 +0800

    added smooth transition

--- src/home/_components/home-hero-section.tsx
+++ src/home/_components/home-hero-section.tsx
@@ -1,6 +1,5 @@
 
-
-
+import { motion } from "framer-motion";
 
 export default function HomeHeroSection(){
 
@@ -9,15 +8,61 @@ export default function HomeHeroSection(){
   return (<>
 
 
-    <div className="flex flex-col border border-[#50B498] green z-10 my-[5em]">
+    <div className="flex flex-col border border-[#50B498] green z-10 my-[5em] ">
 
 
-      <p className="font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase text-[#FF5E3A] border border-[#50B498] green">One scene at a time - the movie maker director.</p>
+      <motion.p className="font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase text-[#FF5E3A] border border-[#50B498] green transition-all duration-1000 ease-in-out "
+      
+      
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+      
+      
+      >One scene at a time - the movie maker director.</motion.p>
 
-      <p className="font-roboto-condensed text-[4rem] lg:text-[6rem] not-italic font-bold leading-[1em] lg:max-w-[8em] text-white border border-[#50B498] green">Behind Every Great Film is a Visionary.</p>
+      <motion.p className="font-roboto-condensed text-[4rem] lg:text-[6rem] not-italic font-bold leading-[1em] lg:max-w-[8em] text-white border border-[#50B498] green"
+      
+      
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 1, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+        
+      
+      >Behind Every Great Film is a Visionary.</motion.p>
 
 
-      <p className="font-source-sans-pro text-white text-[1.25rem] not-italic font-normal border border-[#50B498] green lg:max-w-[29em] mt-[1.875em]">Crafting unforgettable stories bringing their creative vision to life on the big screen - the movie maker director</p>
+      <motion.p className="font-source-sans-pro text-white text-[1.25rem] not-italic font-normal border border-[#50B498] green lg:max-w-[29em] mt-[1.875em]"
+      
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 1, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+      
+      >Crafting unforgettable stories bringing their creative vision to life on the big screen - the movie maker director</motion.p>
 
     
       <div className="flex items-center mt-[1.875em]">
@@ -25,7 +70,22 @@ export default function HomeHeroSection(){
 
         <div className="flex flex-col">
 
-          <button className="py-[1.0625em] px-[1.5625em] bg-[#FF5E3A] text-white rounded-[0.1875em] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">Get started</button>
+          <motion.button className="py-[1.0625em] px-[1.5625em] bg-[#FF5E3A] text-white rounded-[0.1875em] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase"
+          
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: { 
+                opacity: 1, 
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Applying duration and easing
+              },
+            }}
+          
+          
+          >Get started</motion.button>
 
         </div>
 
@@ -34,13 +94,28 @@ export default function HomeHeroSection(){
         <div className="flex flex-col py-[1.0625em] px-[1.5625em] cursor-pointer ">
 
 
-          <div className="flex  items-center justify-between gap-[0.625em] ">
+          <motion.div className="flex  items-center justify-between gap-[0.625em] "
+          
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: { 
+                opacity: 1, 
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Applying duration and easing
+              },
+            }}
+          
+          
+          >
 
             <p className="font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase text-white">view our works</p>
 
             <img src="home-hero-section-play.svg" alt="" width={13} height={0} className="min-w-[0.8125em] mt-[-0.2em]  "/>
 
-          </div>
+          </motion.div>
 
 
 
