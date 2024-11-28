def47781fdb6ff5b1388fa03362f1d88c8767be1
apollo <apollomacabodbod8@gmail.com>
2024-11-29 06:13:35 +0800

    reset to default remove useLocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./home/page";
 import About from "./about/page";
 import Contact from "./contact/page";
