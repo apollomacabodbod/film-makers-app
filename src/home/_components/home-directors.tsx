3302a5097220c3d6036942a4e64901525383b723
apollo <apollomacabodbod8@gmail.com>
2024-11-29 10:04:03 +0800

    added text in home-directors.tsx

--- src/home/_components/home-directors.tsx
+++ src/home/_components/home-directors.tsx
@@ -1,7 +1,7 @@
 
 
 
-
+import { motion } from "framer-motion";
 
 export default function HomeDirectors(){
 
@@ -35,8 +35,68 @@ export default function HomeDirectors(){
 
         <div className="relative bg-[#11121E] w-full h-[16.5em] md:top-[16em] top-[10em]"
 
-
         >
+
+
+          <div className="flex flex-col">
+
+
+            <div className="flex items-center justify-between mt-[3.125em]">
+
+
+              <div className="flex flex-col border border-[#50B498] green">
+
+
+                <motion.div className="flex items-center  gap-[0.625em] transiton-all duration-1000 ease-in-out"
+
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, y: 10 },
+                    visible: { 
+                      opacity: 1, 
+                      y: 0,
+                      transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+                    },
+                  }}
+  
+                >
+
+                  <img src="/home-about-us-line.svg" alt="" width={40} height={0} className="min-w-[2.5em]"/>
+
+                  <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase ">Directors</p>
+
+
+                </motion.div>
+
+
+
+                <p className="text-white font-roboto-condensed text-[4.5rem] not-italic font-bold  leading-[1em] whitespace-nowrap">Meet the Our</p>
+                <p className="text-white font-roboto-condensed text-[4.5rem] not-italic font-bold  leading-[1em] whitespace-nowrap">Video Directors</p>
+
+
+              </div>
+
+
+              <div className="flex flex-col border border-[#50B498] green">
+
+
+                <p className="text-white font-roboto-condensed text-[1.5rem] not-italic font-bold ">Our team is made up of experienced and talented filmmakers who share a common goal</p>
+
+                <p className="text-white">Proin et magna blandit arcu pellentesque scelerisque sit amet a sapien. Aenean purus nunc cursus in ante in.</p>
+
+              </div>
+
+
+            </div>
+
+
+
+    
+          </div>
+
+           
 
 
         </div>
