bfadcb1b3ce6db627b41806bdb72fcff251676b8
apollo <apollomacabodbod8@gmail.com>
2024-11-29 09:49:14 +0800

    added image in home-directors.tsx

--- src/home/_components/home-directors.tsx
+++ src/home/_components/home-directors.tsx
@@ -8,23 +8,33 @@ export default function HomeDirectors(){
 
   return (<>
 
-    <div className="flex flex-col mt-[6.25em] border border-[#50B498] green">
+    <div className="flex flex-col mt-[6.25em] mb-[20em] border border-[#50B498] green">
 
 
-      <div className="bg-center bg-no-repeat bg-cover w-full md:h-[31.25em] h-[25.25em]"
+      <div className="bg-right sm:bg-center bg-no-repeat bg-cover w-full md:h-[31.25em] h-[25.25em] pr-[6.25em]"
 
         style={{
-          backgroundImage: "url('/home-directors-image1.svg')"
+          backgroundImage: "url('/operator-working.jpg')"
         }}
         
       >
 
 
-        <div className="absolute bg-center bg-no-repeat bg-cover w-[6.25em] h-[6.25em]"
+        <div className="relative bg-center bg-no-repeat bg-cover w-[6.25em] h-[6.25em]"
 
           style={{
             backgroundImage: "url('/home-directors-image2.svg')"
           }}
+
+        >
+
+
+        </div>
+
+
+
+        <div className="relative bg-[#11121E] w-full h-[16.5em] md:top-[16em] top-[10em]"
+
 
         >
 
