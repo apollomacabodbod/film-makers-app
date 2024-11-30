9e6aa7dae8231d56708340902eb290cecfdf93f5
apollo <apollomacabodbod8@gmail.com>
2024-11-30 08:36:19 +0800

    added cards images

--- src/home/_components/home-works.cards.tsx
+++ src/home/_components/home-works.cards.tsx
@@ -17,10 +17,10 @@ export default function HomeWorksCards(){
 
    const [data] = useState<HomeCards[]>([
 
-     {id: 1 , title: "TV Commecial", description: 'Proin et magna blandit arcu pellentes scelerisque.', image: '/home-works-card.svg',year: "2023"},
-     {id: 2 , title: "Indie Movie", description: 'Proin et magna blandit arcu pellentes sceleris ameta.', image: '/home-works-card.svg',year: "2023"},
-     {id: 3 , title: "Documentary", description: 'Proin et magna blandit arcu pellentes sceleris ameta.', image: '/home-works-card.svg',year: "2023"},
-     {id: 4 , title: "Company Profile", description: 'Proin et magna blandit arcu pellentes sceleris ameta.', image: '/home-works-card.svg',year: "2023"},
+     {id: 1 , title: "TV Commecial", description: 'Proin et magna blandit arcu pellentes scelerisque.', image: '/romantic-date.jpg',year: "2023"},
+     {id: 2 , title: "Indie Movie", description: 'Proin et magna blandit arcu pellentes sceleris ameta.', image: '/mime-artists.jpg',year: "2023"},
+     {id: 3 , title: "Documentary", description: 'Proin et magna blandit arcu pellentes sceleris ameta.', image: '/portrait-of-videographer.jpg',year: "2023"},
+     {id: 4 , title: "Company Profile", description: 'Proin et magna blandit arcu pellentes sceleris ameta.', image: '/movie-clapper.jpg',year: "2023"},
 
 
    ])
@@ -44,15 +44,17 @@ export default function HomeWorksCards(){
          >
 
 
-           <div className="relative flex flex-col border border-[#50B498] green top-[13.4em] sm:top-[12.9em] sm1:top-[14.4em] py-[1.25em] px-[1.875em]">
+           <div className="relative flex flex-col border border-[#50B498] green top-[8em] sm0:top-[7.3em] sm1:top-[8.5em] sm:top-[8.6em] 
+           
+           pt-[4.8125em] pb-[2em] px-[1.875em] bg-[linear-gradient(180deg,rgba(17,18,30,0)_0%,rgba(17,18,30,0.8)_100%)]">
 
-             <p>{item.title}</p>
+             <p className={`font-roboto-condensed text-white text-[2.25rem] not-italic font-bold leading-[1em] ${item.title === "Company Profile" && "whitespace-nowrap"}`}>{item.title}</p>
 
 
-             <div className="flex items-center justify-between">
+             <div className="flex items-center justify-between mt-[0.5em]">
 
-               <p className="text-[0.8rem]  sm:text-[1rem] sm1:text-[0.8rem] xl:text-[1rem]">{item.description}</p>
-               <p>{item.year}</p>
+               <p className="text-[0.8rem]  sm:text-[1rem] sm1:text-[0.8rem] xl:text-[1rem] text-white font-inter  not-italic font-normal">{item.description}</p>
+               <p className="font-roboto-condensed text-[1.25rem] not-italic font-bold text-white">{item.year}</p>
 
              </div>
 
