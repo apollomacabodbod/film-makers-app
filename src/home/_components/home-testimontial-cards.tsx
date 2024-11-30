87a429f7fc3ecd54d5375861d026f3d6e21668c1
apollo <apollomacabodbod8@gmail.com>
2024-11-30 15:38:42 +0800

    added new card in testimontials

--- src/home/_components/home-testimontial-cards.tsx
+++ src/home/_components/home-testimontial-cards.tsx
@@ -7,36 +7,93 @@ export default function HomeTestimontialCards(){
   return <>
 
 
-    <div className="flex flex-col border border-[#50B498] green bg-white w-[27.1875em] px-[1.875em] py-[1.875em] gap-[1.25em] rounded-[0.3125em] 
+
+    <div className="flex flex-col">
+
+
+      <div className="flex items-center gap-[1.875em]">
+
+
+
+        <div className="flex flex-col border border-[#50B498] green bg-white w-[27.1875em] px-[1.875em] py-[1.875em] gap-[1.25em] rounded-[0.3125em] 
     
     mt-[1.875em]
     ">
 
 
-      <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between">
 
-        <img src="/home-testimontials-rating.svg" alt="" width={79} height={14}/>
-        <img src="/home-testimontials-quotes.svg" alt="" width={32} height={26}/>
+            <img src="/home-testimontials-rating.svg" alt="" width={79} height={14}/>
+            <img src="/home-testimontials-quotes.svg" alt="" width={32} height={26}/>
 
-      </div>
+          </div>
 
-      <p className="text-[#11121E] font-roboto-condensed not-italic font-bold leading-[1em] text-[2.25rem] ">I Really Like the Idea of the Movies</p>
+          <p className="text-[#11121E] font-roboto-condensed not-italic font-bold leading-[1em] text-[2.25rem] ">I Really Like the Idea of the Movies</p>
 
-      <p className="text-[#82869A] font-inter text-[1rem] not-italic font-normal" >Proin magna blandit arcu pellent esque sceleri sque sit amet sapien purus nunc cursus antein vehicula facilisis Integer conse tetur est id.</p>
+          <p className="text-[#82869A] font-inter text-[1rem] not-italic font-normal" >Proin magna blandit arcu pellent esque sceleri sque sit amet sapien purus nunc cursus antein vehicula facilisis Integer conse tetur est id.</p>
    
-      <div className="flex items-center ">
+          <div className="flex items-center ">
 
-        <img src="/home-testimontials-circle.svg" alt="" width={60} height={60} className="mr-[0.9375em]"/>
+            <img src="/home-testimontials-circle.svg" alt="" width={60} height={60} className="mr-[0.9375em]"/>
 
-        <p className="text-[#11121E] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">John Williams</p>
+            <p className="text-[#11121E] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">John Williams</p>
            
-        <img src="/home-testimontials-dash.svg" alt="" width={4} height={15} className="mx-[0.3125em]"/>
+            <img src="/home-testimontials-dash.svg" alt="" width={4} height={15} className="mx-[0.3125em]"/>
 
-        <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">Business</p>
+            <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">Business</p>
+
+          </div>
+     
+        </div>
+
+
+        {/* Component two */}
+
+
+
+        <div className="flex flex-col border border-[#50B498] green bg-white w-[27.1875em] px-[1.875em] py-[1.875em] gap-[1.25em] rounded-[0.3125em] 
+    
+    mt-[1.875em]
+    ">
+
+
+          <div className="flex items-center justify-between">
+
+            <img src="/home-testimontials-rating.svg" alt="" width={79} height={14}/>
+            <img src="/home-testimontials-quotes.svg" alt="" width={32} height={26}/>
+
+          </div>
+
+          <p className="text-[#11121E] font-roboto-condensed not-italic font-bold leading-[1em] text-[2.25rem] ">I Really Like the Idea of the Movies</p>
+
+          <p className="text-[#82869A] font-inter text-[1rem] not-italic font-normal" >Proin magna blandit arcu pellent esque sceleri sque sit amet sapien purus nunc cursus antein vehicula facilisis Integer conse tetur est id.</p>
+   
+          <div className="flex items-center ">
+
+            <img src="/home-testimontials-circle.svg" alt="" width={60} height={60} className="mr-[0.9375em]"/>
+
+            <p className="text-[#11121E] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">John Williams</p>
+           
+            <img src="/home-testimontials-dash.svg" alt="" width={4} height={15} className="mx-[0.3125em]"/>
+
+            <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">Business</p>
+
+          </div>
+     
+        </div>
+
+
+
 
       </div>
-     
+
+
+
+
     </div>
+
+
+  
     
   </>
 }
