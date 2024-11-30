72a10c4db726cd23ee8c6616e7c9e6a54e791a93
apollo <apollomacabodbod8@gmail.com>
2024-11-30 20:49:54 +0800

    added svg in testimontials

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -77,14 +77,14 @@ export default function Home() {
 
 
           <div
-            className="relative lg:z-10 lg:absolute lg:top-[6.4em] lg:right-[3.8%] xl:top-[5.8em] xl:right-[7%] bg-center bg-no-repeat bg-cover w-full lg:w-[20em] h-[20em] lg:h-[35.8em] mt-[2em] lg:mt-[0em]"
+            className="relative lg:z-10 lg:absolute lg:top-[6.4em] lg:right-[3.8%] xl:top-[5.8em] xl:right-[7%] bg-center bg-no-repeat bg-cover w-full lg:w-[20em] h-[35em] lg:h-[35.8em] mt-[2em] lg:mt-[0em]"
             style={{
-              backgroundImage: "url('/home-testimontials-image.svg')",
+              backgroundImage: "url('/videographer-man1.jpg')",
             }}
           >
             {/* Ornament */}
             <div
-              className="absolute bg-center bg-no-repeat bg-cover w-[6.25em] h-[6.25em] top-[13.7em] right-[-0.1%] lg:top-[29.6em] lg:right-[-0.1%]"
+              className="absolute bg-center bg-no-repeat bg-cover w-[6.25em] h-[6.25em] top-[28.7em] right-[-0.1%] lg:top-[29.6em] lg:right-[-0.1%]"
               style={{
                 backgroundImage: "url('/home-testimontials-ornament.svg')",
               }}
