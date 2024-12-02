64897a28474d51861307505fa3567acd7d18babb
apollo <apollomacabodbod8@gmail.com>
2024-12-02 21:18:21 +0800

    adjust text in small screens

--- src/home/_components/home-collaborating.tsx
+++ src/home/_components/home-collaborating.tsx
@@ -4,7 +4,7 @@
 export default function HomeCollaborating() {
   return (
     <>
-      <div className="relative flex flex-col border border-[#50B498] h-[400px] bg-[#BDBDBD] rounded-[5px] mt-[5.875em]">
+      <div className="relative flex flex-col border border-[#50B498] green h-[400px] bg-[#BDBDBD] rounded-[5px] mt-[5.875em]">
         {/* Overlay */}
         <div className="absolute inset-0 bg-gradient-to-r from-[rgba(26,27,43,0.8)] to-[rgba(26,27,43,0)]"></div>
 
@@ -19,7 +19,15 @@ export default function HomeCollaborating() {
           <p className="text-white font-roboto-condensed text-[0.875em] font-bold uppercase">
                         collaborating
           </p>
+
+          
         </div>
+
+        <p className="text-white z-10 font-roboto-condensed text-[4.5rem] xl:text-[4.5rem] not-italic font-bold leading-[1em] lg:max-w-[10em] border border-[#50B498] green">We Love Collaborating With Other Creatives</p>
+
+
+
+
       </div>
     </>
   );
