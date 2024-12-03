671e7d09b80373b9f942d4589735e152a1370aa3
apollo <apollomacabodbod8@gmail.com>
2024-12-04 05:28:01 +0800

    added margin top in footer cards

--- src/home/_components/home-footer-cards.tsx
+++ src/home/_components/home-footer-cards.tsx
@@ -13,11 +13,11 @@ export default function HomeFooterCards(){
 
 
 
-        <div className="flex flex-col mt-[1em]">
+        <div className="flex flex-col ">
 
           <p className="text-white font-roboto-condensed text-[1.25rem] not-italic font-bold ">Services</p>
 
-          <p className="text-[#82869A] font-inter text-[1rem] not-italic font-normal whitespace-nowrap">Movie production</p>
+          <p className="text-[#82869A] mt-[0.625em] lg:mt-[0em] font-inter text-[1rem] not-italic font-normal whitespace-nowrap">Movie production</p>
 
           <p className="text-[#82869A] mt-[0.625em] font-inter text-[1rem] not-italic font-normal whitespace-nowrap">Movie directing</p>
 
@@ -33,11 +33,11 @@ export default function HomeFooterCards(){
 
 
 
-        <div className="flex flex-col max-w-[16.4375em] mt-[1.5em]">
+        <div className="flex flex-col max-w-[16.4375em] ">
 
           <p className="text-white font-roboto-condensed text-[1.25rem] not-italic font-bold ">Quick links</p>
 
-          <p className="text-[#82869A]  font-inter text-[1rem] not-italic font-normal">About</p>
+          <p className="text-[#82869A] mt-[0.625em] lg:mt-[0em] font-inter text-[1rem] not-italic font-normal">About</p>
 
           <p className="text-[#82869A] mt-[0.625em] font-inter text-[1rem] not-italic font-normal">Services</p>
 
@@ -51,11 +51,11 @@ export default function HomeFooterCards(){
 
 
 
-        <div className="flex flex-col max-w-[16.4375em] mt-[1.5em]">
+        <div className="flex flex-col max-w-[16.4375em] ">
 
           <p className="text-white font-roboto-condensed text-[1.25rem] not-italic font-bold ">Supports</p>
 
-          <p className="text-[#82869A]  font-inter text-[1rem] not-italic font-normal whitespace-nowrap">Help center</p>
+          <p className="text-[#82869A] mt-[0.625em] lg:mt-[0em] font-inter text-[1rem] not-italic font-normal whitespace-nowrap">Help center</p>
 
           <p className="text-[#82869A] mt-[0.625em] font-inter text-[1rem] not-italic font-normal">Disclaimer</p>
 
