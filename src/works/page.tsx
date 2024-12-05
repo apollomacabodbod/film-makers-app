3458c72f703eb87155ecdc9d7c56eb5f24d0cfe4
apollo <apollomacabodbod8@gmail.com>
2024-12-05 13:40:28 +0800

    added works component

--- src/works/page.tsx
+++ src/works/page.tsx
@@ -1,3 +1,4 @@
+import WorksOurWorks from "./_components/works-our-works";
 
 
 
@@ -10,7 +11,7 @@ export default function Works(){
 
     <div className="flex flex-col  bg-[#232639] xl:px-[5.5em] lg:px-[2.75em] px-[1.25em] transition-all duration-1000 ease-in-out">
 
-
+      <WorksOurWorks/>
 
     </div>
 
