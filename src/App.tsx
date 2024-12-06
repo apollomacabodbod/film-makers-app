11ddb4b6983425290f8068b7d2b993c13a21c3dc
apollo <apollomacabodbod8@gmail.com>
2024-12-06 15:02:15 +0800

    re-mount the page when it enters the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./home/page";
 import About from "./about/page";
 import Contact from "./contact/page";
@@ -7,53 +8,35 @@ import HomeFooter from "./home/_components/home-footer";
 import Services from "./our-services/page";
 import Works from "./works/page";
 
-function App() {
+function ScrollToTop() {
+  const location = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null; // This component doesn't render anything
+}
+
+function App() {
+  const location = useLocation(); // Now safe to use because it's inside a Router
 
   return (
-
     <>
-    
-     
-
-    
-      <Router>
-     
-    
-        <HomeHeader/>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/works" element={<Works/>} />
-          
-        </Routes>
-
-
-
-        < div
-          className="flex flex-col  bg-[#11121E] xl:px-[5.5em] lg:px-[2.75em] px-[1.25em]  "
-        >
-
-
-          <HomeFooter/>
-
-
-        </div>
-
-
-
-      </Router>
-
-
-
-
+      <ScrollToTop /> {/* Handles scroll-to-top on location change */}
+      <HomeHeader />
+      
+      <Routes > {/* Unique key for re-mounting */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/works" element={<Works />} />
+      </Routes>
+      <div key={location.pathname} className="flex flex-col bg-[#11121E] xl:px-[5.5em] lg:px-[2.75em] px-[1.25em]">
+        <HomeFooter />
+      </div>
     </>
-
-
-
   );
 }
 
