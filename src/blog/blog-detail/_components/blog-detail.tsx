7afd846b909102d56cc4f68e5739c7ad037fe6ac
apollo <apollomacabodbod8@gmail.com>
2024-12-10 14:24:48 +0800

    added comment component

--- src/blog/blog-detail/_components/blog-detail.tsx
+++ src/blog/blog-detail/_components/blog-detail.tsx
@@ -1,4 +1,7 @@
 import { useParams } from "react-router-dom";
+import { motion } from 'framer-motion'
+
+
 
 export default function BlogDetail() {
   const { id } = useParams();
@@ -6,18 +9,18 @@ export default function BlogDetail() {
   return (<>
 
 
-    <div className="flex flex-col border border-[#50B498] green mt-[3.125em] mb-[6.25em] max-w-[48.125em] gap-[2em]">
+    <div className="flex flex-col border border-[#50B498] green mt-[3.125em] mb-[6.25em] max-w-[48.125em] ">
 
       <p className="text-[#82869A] font-roboto-condensed text-[1.5rem] not-italic font-bold leading-[1.2em]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-      <p className="text-[#82869A] font-roboto text-[1rem] not-italic font-normal">Bibendum sit pharetra vitae pellentesque sodales neque mollis orci, purus. Feugiat volutpat eu velit purus enim cum. Eu ipsum viverra sudisse placerat leo. Libero vel mi porttitor luctus viverra a vel vehicula ultricies. Tellus egestas ac in sit et quis vitae. Metus auctor aliquam id cursus quam tempor vulputate cursus.<br/><br/>
+      <p className="text-[#82869A] font-roboto text-[1rem] not-italic font-normal mt-[2em]">Bibendum sit pharetra vitae pellentesque sodales neque mollis orci, purus. Feugiat volutpat eu velit purus enim cum. Eu ipsum viverra sudisse placerat leo. Libero vel mi porttitor luctus viverra a vel vehicula ultricies. Tellus egestas ac in sit et quis vitae. Metus auctor aliquam id cursus quam tempor vulputate cursus.<br/><br/>
 
        Bibendum leo, sapien, nisl bibendum. Ultricies urna ultricies risus, at. Risus tincidunt netus et pellentesque maecenas pulvinar. Arcu duis tortor turpis nunc, nunc est blandit. Vel volutpat purus mauris condimentum amet tellus scelerisque semper blandit. Feugiat accumsan blandit acu, integer faucibus. At cursus elit hac vitae nec tortor iaculis viverra. Sem aliquet vulputate metus tristique sem commodo faucibus. Sagittis sittis eget at sit posuere est at. Eget eget id iaculis at est cras tristique sit. Nunc sem magna morbi vitae tempus sed consequat. Urna tellus id tique mattis molestie lectus imperdiet. Facilisi suscipit mauris nisl nibh felis pellentesque. Tempus et vitae, at quis nisl enim molestie felis. Tincidunt ornare facilisis interdum ipsum.</p>
 
 
-      <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase leading-[1.2em]">AI,  Machine Enginer,  Computer Vision,  3D Vision</p>
+      <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase leading-[1.2em] mt-[2em]">AI,  Machine Enginer,  Computer Vision,  3D Vision</p>
  
  
-      <div className="flex items-center border border-[#50B498] green gap-[0.625em]">
+      <div className="flex items-center border border-[#50B498] green gap-[0.625em] mt-[2em]">
 
 
         <div className="flex flex-col w-full">
@@ -80,9 +83,40 @@ export default function BlogDetail() {
         </div>
 
 
+     
+
 
       </div>
 
+
+
+      <motion.div className="flex items-center  gap-[0.625em] transiton-all duration-1000 ease-in-out mt-[6.25em]"
+            
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+                      
+      >
+            
+        <img src="/home-about-us-line.svg" alt="" width={40} height={0} className="min-w-[2.5em]"/>
+            
+        <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase ">comments</p>
+            
+            
+      </motion.div>
+
+
+      <p className="text-white font-roboto-condensed text-[4.5rem] not-italic font-bold leading-[1em] ">Leave a Reply</p>
+
+      <p className="text-[#82869A] mt-[1.875em]">Your email address will not be published. Required fields are marked *</p>
  
     </div>
 
