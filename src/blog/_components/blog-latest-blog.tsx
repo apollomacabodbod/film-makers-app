c9677180bc313c495c8a5701200ae9f8b3e62f67
apollo <apollomacabodbod8@gmail.com>
2024-12-12 15:26:35 +0800

    adjust cards in medium screens and large screens

--- src/blog/_components/blog-latest-blog.tsx
+++ src/blog/_components/blog-latest-blog.tsx
@@ -32,7 +32,7 @@ export default function LatestBlogCards(){
 
   return (<>
 
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[1.875em]  mb-[6.25em] '>
+    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  gap-[1.875em]  mb-[6.25em] '>
 
       {latestcards.map((data) => (
 
