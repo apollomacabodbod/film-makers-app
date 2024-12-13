353d209fbbc5e25543ddae19fc004b6fd52b6c78
apollo <apollomacabodbod8@gmail.com>
2024-12-13 10:00:26 +0800

    added testimontials component

---
+++ src/testimontials/_components/testimontials-detail.tsx
@@ -0,0 +1,91 @@
+
+
+import { motion } from 'framer-motion'
+
+export default function TestimontialsDetail(){
+
+  return (<>
+
+
+    <div className="flex flex-col  lg:max-w-[35em] xl:max-w-[40.625em] transition-all duration-1000 ease-in-out">
+
+      <motion.div className="flex items-center  gap-[0.625em] transiton-all duration-1000 ease-in-out"
+            
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+              
+      >
+    
+        <img src="/home-about-us-line.svg" alt="" width={40} height={0} className="min-w-[2.5em]"/>
+    
+        <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase ">testimonials</p>
+    
+    
+      </motion.div>
+
+
+      <motion.p className='text-white font-roboto-condensed text-[2.5rem] sm3:text-[4rem] lg:text-[4.5rem] not-italic font-bold lg:whitespace-nowrap leading-[1em] mt-[0.2em] lg:mt-[0em] transiton-all duration-1000 ease-in-out'
+      
+      
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+      
+      >What People Says?</motion.p>
+
+      <motion.p className='text-[#82869A] font-roboto-condensed text-[1.5rem] font-bold not-italic mt-[1em] lg:mt-[0em] transiton-all duration-1000 ease-in-out'
+      
+       
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+      
+      
+      >What they say about us?</motion.p>
+      <motion.p className='text-[#82869A] font-inter text-[1rem] not-italic font-normal mt-[1em] lg:mt-[0em] transiton-all duration-1000 ease-in-out'
+      
+      
+       
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { 
+            opacity: 1, 
+            y: 0,
+            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+          },
+        }}
+      
+      >Proin et magna blandit arcu pellentesque scelerisque sit amet a sapien. Aenean purus nunc, cursus in ante in, vehicula facilisis dui. Integer consequat consectetur est id</motion.p>
+    </div>
+    
+    
+  </>)
+}
--- src/testimontials/page.tsx
+++ src/testimontials/page.tsx
@@ -1,4 +1,6 @@
-import WorksHeroSection from "../works/_components/works-hero-section";
+import TestimontialCards from "./_components/testimontials-cards";
+import TestimontialsDetail from "./_components/testimontials-detail";
+import TestimontialsHeroSection from "./_components/testimontials-hero-section";
 
 
 
@@ -21,10 +23,68 @@ export default function Testimontials(){
       <div className="absolute inset-0 bg-black opacity-20"></div>
     
           
-      <WorksHeroSection/>
+      <TestimontialsHeroSection/>
+    
+    </div>
+
+
+
+
+    <div className="flex flex-col  bg-no-repeat h-auto bg-[#232639] "
+    
+      style={{
+        backgroundImage: "url('/home-testimontials-vector1.svg')"
+      }}
+    
+    
+    >
+    
+      <div className=" lg:relative   flex flex-col bg-no-repeat h-auto  px-[1.25em] transition-all duration-1000 ease-in-out py-[6.25em] "
+    
+        style={{
+          backgroundImage: "url('/home-testimontials-vector2.svg')"
+        }}
+    
+      >
+    
+        <div className="max-w-[1200px] lg:mx-auto lg:px-[1.25em] ">
+    
+    
+          {/* components */}
+          <TestimontialsDetail/>
+          <TestimontialCards/>
+    
+        </div>
+    
+    
+        <div
+          className=" relative lg:z-10 lg:absolute lg:top-[6.1em] lg:right-[3.8%] xl:top-[5.5em] xl:right-[7%] bg-top bg-no-repeat bg-cover w-full lg:w-[20em] h-[26em] lg:h-[35.8em] mt-[2em] lg:mt-[0em] rounded-[0.3125em]"
+          style={{
+            backgroundImage: "url('/videographer-man1.jpg')",
+          }}
+        >
+          {/* Ornament */}
+          <div
+            className="absolute bg-center bg-no-repeat bg-cover w-[6.25em] h-[6.25em] top-[19.8em] right-[-0.1%] lg:top-[29.6em] lg:right-[-0.1%]"
+            style={{
+              backgroundImage: "url('/home-testimontials-ornament.svg')",
+            }}
+          ></div>
+        </div>
+    
+    
+    
+    
+      </div>
+    
     
     
     </div>
+
+
+
+
+
     
     
   </>)
