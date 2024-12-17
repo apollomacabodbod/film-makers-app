d1ea95cd1c5954d4fa2190cbf59518e2011f8596
apollo <apollomacabodbod8@gmail.com>
2024-12-18 06:42:02 +0800

    added back to home button

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 
 export default function NotFound(){
@@ -37,7 +38,6 @@ export default function NotFound(){
     
         >
 
-          <img src="/home-about-us-line.svg" alt="" width={40} height={0} className="min-w-[2.5em]"/>
 
           <p className="text-[#FF5E3A] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">page not found</p>
 
@@ -87,6 +87,38 @@ export default function NotFound(){
           <p className="font-source-sans-pro text-[1.25rem] not-italic  text-white font-normal">Sorry, we couldn’t find the page you are looking for.</p>
 
         </motion.div>
+
+
+        <motion.div className="flex items-center justify-center mt-[1.875em] transiton-all duration-1000 ease-in-out"
+            
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+            },
+          }}
+          
+          
+        >
+
+  
+          <Link to={"/"} >
+
+            <button className="py-[1.0625em] px-[1.5625em] bg-[#FF5E3A] text-white rounded-[0.1875em] font-roboto-condensed text-[0.875rem] not-italic font-bold uppercase">back to home</button>
+
+          </Link>
+
+           
+
+
+        </motion.div>
+
+
 
 
       </div>
